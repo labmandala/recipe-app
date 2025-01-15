9bd78e1858a881fe9b2ffe99313dcae355fc9af9
sarah <skg@neofilm.us>
2025-01-14 20:19:33 -0500

    Create & configure SQLite database

--- api/api.py
+++ api/api.py
@@ -3,6 +3,28 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+db = SQLAlchemy(app)
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True,
+                            default='Delicious. You need to try it!')
+    image_url = db.Column(db.String(500), nullable=True,
+                          default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+    servings = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
+
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == '__main__':
