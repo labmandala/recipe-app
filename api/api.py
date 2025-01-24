32db35c734c6ecfb63c5009b0c6f5fd6a57eeb74
sarah <skg@neofilm.us>
2025-01-24 17:33:13 -0500

    Create & test PUT endpoint

--- api/api.py
+++ api/api.py
@@ -26,6 +26,8 @@ class Recipe(db.Model):
 #    db.session.commit()
 
 # Route to fetch all recipes
+
+
 @app.route('/api/recipes', methods=['GET'])
 def get_all_recipes():
     recipes = Recipe.query.all()
@@ -43,6 +45,8 @@ def get_all_recipes():
     return jsonify(recipe_list)
 
 # Route to add a new recipe
+
+
 @app.route('/api/recipes', methods=['POST'])
 def add_recipe():
     data = request.get_json()
@@ -79,5 +83,45 @@ def add_recipe():
 
     return jsonify({'message': 'Recipe added successfully', 'recipe': new_recipe_data})
 
+# Route to update a recipe
+
+
+@app.route('/api/recipes/<int:recipe_id>', methods=['PUT'])
+def update_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+
+    data = request.get_json()
+    
+   # Validate the incoming JSON data for required fields
+    required_fields = ['title', 'ingredients',
+                      'instructions', 'servings', 'description', 'image_url']
+
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
+
+    recipe.title = data['title']
+    recipe.ingredients = data['ingredients']
+    recipe.instructions = data['instructions']
+    recipe.servings = data['servings']
+    recipe.description = data['description']
+    recipe.image_url = data['image_url']
+
+    db.session.commit()
+ 
+    updated_recipe = {
+        'id': recipe.id,
+        'title': recipe.title,
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'servings': recipe.servings,
+        'description': recipe.description,
+        'image_url': recipe.image_url
+    }
+    
+    return jsonify({'message': 'Recipe updated successfully', 'recipe': updated_recipe})
+        
 if __name__ == '__main__':
     app.run(debug=True)
