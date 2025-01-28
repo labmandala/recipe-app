720c929da17009abd7c0d5e9b64e3a0df97895b9
sarah <skg@neofilm.us>
2025-01-27 19:46:43 -0500

    Fetch all the recipes, output state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import "./App.css";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes");
+        if (response.ok) {
+          const data = await response.json();
+          setRecipes(data);
+        } else {
+          console.log("Oops - could not fetch recipes!");
+        }
+      } catch (e) {
+        console.error("An error occurred during the request:", e);
+        console.log("An unexpected error occurred. Please try again later.");
+      }
+    };
+    fetchAllRecipes();
+  }, []);
+
   return (
     <div className='recipe-app'>
       <Header />
-      <p>Your recipes here! </p>
+      {JSON.stringify(recipes)}
     </div>
   );
 }
