d8e2eba167ccd8431bb299bcf7ff7cacd88b0d73
sarah <skg@neofilm.us>
2025-01-30 15:50:39 -0500

    Show full selected recipe component feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
+import RecipeFull from "./components/RecipeFull";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
   
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -24,14 +26,25 @@ function App() {
     fetchAllRecipes();
   }, []);
 
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null);
+  };
+
   return (
     <div className='recipe-app'>
       <Header />
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+      {!selectedRecipe && (
       <div className="recipe-list">
         {recipes.map((recipe) => (
-          <RecipeExcerpt key={recipe.id} recipe={recipe} />
+          <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />
         ))}
       </div>
+      )}
     </div>
   );
 }
