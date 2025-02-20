b8953325188998d46d6a16450dbc0110ac6bc740
sarah <skg@neofilm.us>
2025-02-20 18:32:16 -0500

    Editing form field value properties

--- src/components/RecipeFull.js
+++ src/components/RecipeFull.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import EditRecipeForm from "./EditRecipeForm";
 import { X } from "react-feather";
 
@@ -11,47 +11,49 @@ const RecipeFull = ({ selectedRecipe, handleUnselectRecipe }) => {
 
   return (
     <div className="recipe-details">
-      {editing? (
-         <EditRecipeForm 
+      {editing ? (
+        <EditRecipeForm
           selectedRecipe={selectedRecipe}
           handleCancel={handleCancel}
-         />
-       ) : (
-      <article>
-        <header>
-          <figure>
-            <img alt={selectedRecipe.title} src={selectedRecipe.image_url} />
-          </figure>
-          <h2>{selectedRecipe.title} </h2>
-          <div className="button-container">
-            <button className="edit-button" onClick={() => setEditing(true)}>Edit</button>
-            <button className="cancel-button" onClick={handleUnselectRecipe}>
-              <X />
-              Close
-            </button>
-            <button className="delete-button">Delete</button>
-          </div>
-        </header>
+        />
+      ) : (
+        <article>
+          <header>
+            <figure>
+              <img alt={selectedRecipe.title} src={selectedRecipe.image_url} />
+            </figure>
+            <h2>{selectedRecipe.title} </h2>
+            <div className="button-container">
+              <button className="edit-button" onClick={() => setEditing(true)}>
+                Edit
+              </button>
+              <button className="cancel-button" onClick={handleUnselectRecipe}>
+                <X />
+                Close
+              </button>
+              <button className="delete-button">Delete</button>
+            </div>
+          </header>
 
-        <h3>Description:</h3>
-        <p>{selectedRecipe.description}</p>
+          <h3>Description:</h3>
+          <p>{selectedRecipe.description}</p>
 
-        <h3>Ingredients:</h3>
+          <h3>Ingredients:</h3>
 
-        <ul className="ingredient-list">
-          {selectedRecipe.ingredients.split(",").map((ingredient, index) => (
-            <li className="ingredient" key={index}>
-              {ingredient}
-            </li>
-          ))}
-        </ul>
-        <h3>Instructions:</h3>
+          <ul className="ingredient-list">
+            {selectedRecipe.ingredients.split(",").map((ingredient, index) => (
+              <li className="ingredient" key={index}>
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+          <h3>Instructions:</h3>
 
-        <pre className="formatted-text">{selectedRecipe.instructions}</pre>
+          <pre className="formatted-text">{selectedRecipe.instructions}</pre>
 
-        <h3>Servings: {selectedRecipe.servings}</h3>
-      </article>
-       )}
+          <h3>Servings: {selectedRecipe.servings}</h3>
+        </article>
+      )}
     </div>
   );
 };
